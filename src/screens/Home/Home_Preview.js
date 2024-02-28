8b6a3f89029bdce95d42f5f88af2bda06ba78509
Kenneth Postigo <7651554+kpostigo@users.noreply.github.com>
2024-02-28 18:55:21 -0500

    added components for user instructions

--- src/screens/Home/Home_Preview.js
+++ src/screens/Home/Home_Preview.js
@@ -87,43 +87,90 @@ const Home_Preview = ({ key }) => {
   return (
     <View style={styles.container}>
       {/* Only showing the last shared song. If none, show instructions */}
-      <View>
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-          <Text style={styles.subtext(theme)}>{songType}</Text>
-          <Text style={styles.subtext(theme)}>from {origin}</Text>
-        </View>
-        <Image
-          style={styles.coverArt}
-          source={{ uri: thumbnail }}
-        />
-      </View>
-      <View style={{ alignItems: 'center', marginBottom: 5 }}>
-        <Text style={styles.titleText(theme)}>{songName}</Text>
-        <Text style={styles.text(theme)}>{artistName}</Text>
-      </View>
-      <FlatList
-        style={styles.flatListStyle}
-        scrollEnabled={false}
-        numColumns={2}
-        data={platformsAvailable}
-        renderItem={renderPlatformsAvailable}
-        keyExtractor={item => item.id}
-        refreshing={loading}
-      />
-      <Pressable
-        onPress={handleNewShare}
-        style={({ pressed }) => [styles.shareButton(pressed)]}
-        android_ripple={styles.androidRipple}
-      >
-        {({ pressed }) => (
-          <Icon
-            name="duplicate-outline"
-            size={28}
-            color={pressed ? '#f2ee6e' : '#776DF2'}
+      {data ? (
+        <>
+          <View>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+              <Text style={styles.subtext(theme)}>{songType}</Text>
+              <Text style={styles.subtext(theme)}>from {origin}</Text>
+            </View>
+            <Image
+              style={styles.coverArt}
+              source={{ uri: thumbnail }}
+            />
+          </View>
+          <View style={{ alignItems: 'center', marginBottom: 5, marginHorizontal: 20 }}>
+            <Text style={[styles.titleText(theme), { textAlign: 'center' }]}>
+              {songName}
+            </Text>
+            <Text style={[styles.text(theme), { textAlign: 'center' }]}>
+              {artistName}
+            </Text>
+          </View>
+          <FlatList
+            style={styles.flatListStyle}
+            scrollEnabled={false}
+            numColumns={2}
+            data={platformsAvailable}
+            renderItem={renderPlatformsAvailable}
+            keyExtractor={item => item.id}
+            refreshing={loading}
           />
-        )}
-      </Pressable>
-    </View>
+        </>
+      ) : (
+        <>
+          <View style={{ position: 'absolute', top: 0, bottom: 100, left: 0, right: 0, justifyContent: 'center', alignItems: 'center' }}>
+            <FastImage
+              source={require('assets/home/beatbridge1-500.png')}
+              style={{
+                height: 200,
+                width: 200,
+                marginBottom: 20,
+              }}
+            />
+            <Text style={[styles.titleText(theme), {
+              textAlign: 'center',
+              marginBottom: 20,
+            }]}>
+              Convert your music link to other services
+            </Text>
+            <View>
+              <Text style={[styles.text(theme), { paddingHorizontal: 24, marginBottom: 4 }]}>
+                1. Copy music link from source (e.g Spotify)*
+              </Text>
+              <Text style={[styles.subtext(theme), { paddingHorizontal: 24, marginVertical: 0 }]}>
+                *Sources supported by Odesli.co
+              </Text>
+              <Text style={[styles.text(theme), { paddingHorizontal: 24, marginTop: 10, marginBottom: 4 }]}>
+                2. Paste link by pressing the <Icon name="arrow-up" size={20} color="black" /> button
+              </Text>
+              <Text style={[styles.subtext(theme), { paddingHorizontal: 24, marginVertical: 0 }]}>
+                Make sure to "Allow Paste"
+              </Text>
+              <Text style={[styles.text(theme), { paddingHorizontal: 24, marginVertical: 10 }]}>
+                3. Copy new link by tapping from the list
+              </Text>
+            </View>
+          </View>
+          <Pressable
+            onPress={handleNewShare}
+            style={({ pressed }) => [styles.shareButton(pressed)]}
+            android_ripple={styles.androidRipple}
+          >
+            {({ pressed }) => (
+              <>
+                <Icon
+                  name="arrow-up"
+                  size={28}
+                  color={pressed ? '#f2ee6e' : '#776DF2'}
+                />
+              </>
+            )}
+          </Pressable>
+        </>
+      )
+      }
+    </View >
   );
 };
 
